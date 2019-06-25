639ab7beeaed5b1cb7cb252bc8b0db3538e349fa
ljoly83 <joly.laurent@gmail.com>
2019-06-25 18:46:09 +0200

    Work on form

--- src/client/ionic/lambulancier/src/app/simulator/ambulance/ambulance.page.ts
+++ src/client/ionic/lambulancier/src/app/simulator/ambulance/ambulance.page.ts
@@ -10,6 +10,19 @@ export class AmbulancePage implements OnInit {
   registerForm: FormGroup;
   submitted = false;
 
+  validationMessages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+  }
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
